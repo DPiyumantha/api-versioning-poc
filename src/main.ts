e00533d48b5584c85b6aeab3f9ed0483d8d0247a
dpiyumantha <dimalka.piyumantha@gmail.com>
2024-08-17 08:57:31 +0530

    changed port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const PORT = 8080;
   const app = await NestFactory.create(AppModule);
 
   app.enableVersioning({
@@ -19,7 +20,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  
-  await app.listen(3000);
+  await app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`);
+  });
 }
 bootstrap();
