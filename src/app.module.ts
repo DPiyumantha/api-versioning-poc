a423d75b2c1efa2bf2770033f99fde2d39c6f08b
dpiyumantha <dimalkapiyumantha@gmail.com>
2024-08-20 10:46:15 +0530

    add contacts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReferencesModule } from './references/references.module';
+import { ContactsModule } from './contacts/contacts.module';
 
 @Module({
-  imports: [ReferencesModule],
+  imports: [ReferencesModule, ContactsModule],
   controllers: [AppController],
   providers: [AppService],
 })
